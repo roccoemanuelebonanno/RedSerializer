e4ed17227055a3112ab178e614332db0fb626f23
Rocco Emanuele Bonanno <roccoemanuelebonanno@live.it>
2024-07-08 00:25:25 +0200

    :feat: add serialization for poro and active_record object

--- lib/red_serializer/serializer.rb
+++ lib/red_serializer/serializer.rb
@@ -4,10 +4,28 @@ class Serializer
   end
 
   def serialize
-    attributes = @object.attribute_names
+    active_record_object? ? serialize_active_record_obj : serialize_poro_obj
+  end
+
+  private
+
+  def active_record_object?
+    @object.class.ancestors.map(&:to_s).include? "ActiveRecord::Base"
+  end
+
+  def serialize_active_record_obj
+    hash = {}
+    @object.attribute_names.each do |attribute|
+      hash[attribute.to_s] = @object[attribute]
+    end
+    hash
+  end
+
+  def serialize_poro_obj
+    attributes = @object.instance_variables.map { |iv| iv.to_s.sub(/^@/, "") }
     hash = {}
     attributes.each do |attribute|
-      hash[attribute.to_sym] = obj[attribute]
+      hash[attribute.to_s] = @object.instance_variable_get "@#{attribute}"
     end
     hash
   end
